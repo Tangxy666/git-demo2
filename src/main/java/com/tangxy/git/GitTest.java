dbc660cf480687269ac816224d55b11e0b5603a9
tangxy <2489086623@qq.com>
2022-07-25 10:53:31 +0800

    second commit

--- src/main/java/com/tangxy/git/GitTest.java
+++ src/main/java/com/tangxy/git/GitTest.java
@@ -3,5 +3,7 @@ package com.tangxy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git2!");
+//        System.out.println("Hello git3!");
     }
 }
